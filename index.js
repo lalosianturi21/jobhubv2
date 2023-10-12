af411f00b55204015af3c2f04b409e9da441fa06
swordxhunter <fulalotio210603@gmail.com>
2023-10-12 16:16:50 +0700

    jobhub first

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express();
+const port = 3000
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const jobRouter = require('./routes/job');
+const bodyParser = require('body-parser');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log('Connect to V2'))
+.catch((err) => console.log(err));
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use('/api/jobs', jobRouter)
+
+
+app.listen(process.env.PORT || port, () => console.log(`The Hub is Listening on Port ${process.env.PORT}`))
